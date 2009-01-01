0fa5facdee62ae3474ddf419087589810b02beda
Ben Scofield <git@turrean.com>
2009-01-01 10:09:09 -0500

    remove irrelevant filter (relic from abstraction)

--- app/controllers/buildouts_controller.rb
+++ app/controllers/buildouts_controller.rb
@@ -1,6 +1,5 @@
 class BuildoutsController < ApplicationController
   skip_before_filter :check_for_buildout
-  skip_before_filter :store_location
   
   def show
     template_name = "buildouts/#{params[:c]}/#{params[:a]}"

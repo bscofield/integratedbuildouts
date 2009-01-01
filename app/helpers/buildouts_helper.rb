7fce9dcc4bac0f52041c56fc54598d2e474f1a1e
Ben Scofield <git@turrean.com>
2009-01-01 10:06:55 -0500

    remove unneeded helper

--- app/helpers/buildouts_helper.rb
+++
@@ -1,7 +0,0 @@
-module BuildoutsHelper
-  def link_to_buildout
-    if Rails.env == 'development' && params[:controller] != 'buildouts'
-      link_to 'view buildout', buildout_path(:c => params[:controller], :a => params[:action])
-    end
-  end
-end
